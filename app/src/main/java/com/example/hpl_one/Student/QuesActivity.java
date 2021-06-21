ce1ec4399aa3658694548ffd4c6751c3c173d599
DauMoe <lehuuyhoang1111999@gmail.com>
2021-06-21 17:35:58 +0700

    (DOING) student mode

--- app/src/main/java/com/example/hpl_one/Student/QuesActivity.java
+++ app/src/main/java/com/example/hpl_one/Student/QuesActivity.java
@@ -1,16 +1,118 @@
 package com.example.hpl_one.Student;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.hpl_one.Modules.Question;
 import com.example.hpl_one.R;
+import com.example.hpl_one.SQLite.SQliteQuestionHelper;
 
-public class QuesActivity extends AppCompatActivity {
+public class QuesActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView ques;
+    private AppCompatButton ans_a, ans_b, ans_c, ans_d, next_ques;
+    private long totalQues = 0;
+    private int currentQuesId = 0;
+    private SQliteQuestionHelper db;
+    private Question x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ques);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initVar();
+    }
+
+    private void initVar() {
+        ques        = findViewById(R.id.student_ques);
+        ans_a       = findViewById(R.id.ans_a);
+        ans_b       = findViewById(R.id.ans_b);
+        ans_c       = findViewById(R.id.ans_c);
+        ans_d       = findViewById(R.id.ans_d);
+        next_ques   = findViewById(R.id.ques_next);
+
+        //Get amount of ques
+        totalQues = db.getQuesAmount();
+        showQues(currentQuesId);
+    }
+
+    private void showQues(int id) {
+        x = db.getQues(id);
+        ques.setText(x.getQues());
+        ans_a.setText("A. "+x.getAnswer_a());
+        ans_a.setBackgroundColor();
+        ans_b.setText("B. "+x.getAnswer_b());
+        ans_c.setText("C. "+x.getAnswer_c());
+        ans_d.setText("D. "+x.getAnswer_d());
+    }
+
+    @Override
+    public void onClick(View v) {
+        String choose;
+        switch (v.getId()) {
+            case R.id.ans_a:
+                checkAns("A");
+                break;
+            case R.id.ans_b:
+                checkAns("B");
+                break;
+            case R.id.ans_c:
+                checkAns("C");
+                break;
+            case R.id.ans_d:
+                checkAns("D");
+                break;
+            case R.id.ques_next:
+                if ((currentQuesId < totalQues)) {
+                    showQues(++currentQuesId);
+                } else {
+                    showQues(0);
+                }
+                break;
+        }
+    }
+
+    private void checkAns(String ans) {
+        switch (x.getCorrect_ans()) {
+            case "A":
+                ans_a.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.correct_ans));
+                break;
+            case "B":
+                ans_b.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.correct_ans));
+                break;
+            case "C":
+                ans_c.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.correct_ans));
+                break;
+            case "D":
+                ans_d.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.correct_ans));
+                break;
+        }
+
+        if (!ans.equalsIgnoreCase(x.getCorrect_ans())) {
+            switch (ans) {
+                case "A":
+                    ans_a.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.error_ans));
+                    break;
+                case "B":
+                    ans_b.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.error_ans));
+                    break;
+                case "C":
+                    ans_c.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.error_ans));
+                    break;
+                case "D":
+                    ans_d.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.error_ans));
+                    break;
+            }
+        }
     }
 }
