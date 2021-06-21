f7caceef27420ca62e623bdec085d347c45c7f13
DauMoe <lehuyhoang1111999@gmail.com>
2021-06-21 23:17:01 +0700

    (DONE) student

--- app/src/main/java/com/example/hpl_one/SQLite/SQliteQuestionHelper.java
+++ app/src/main/java/com/example/hpl_one/SQLite/SQliteQuestionHelper.java
@@ -1,5 +1,6 @@
 package com.example.hpl_one.SQLite;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
@@ -22,7 +23,7 @@ public class SQliteQuestionHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE " + QUES_TABLE + "(" +
+        String sql = "CREATE TABLE question (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "ques TEXT," +
                 "ans_a TEXT," +
@@ -33,20 +34,21 @@ public class SQliteQuestionHelper extends SQLiteOpenHelper {
                 "level TEXT" +
                 ")";
         db.execSQL(sql);
-        insertData();
     }
 
-    private void insertData() {
+    public void insertData() {
+        SQLiteDatabase st = this.getWritableDatabase();
         List<Question> data = new ArrayList<>();
-        SQLiteDatabase st = getWritableDatabase();
         data.add(new Question(0, "In many western cultures, the White Lily is a flower that is representative of purity and sweetness\n What is the same meaning with 'representative'?", "advantageous", "marvelous", "symbolic", "universal", "C", "1"));
         data.add(new Question(1, "I can’t figure out Daisy’s attitude toward the trip – she is blowing hot and cold about it. First she’s all excited, but then she seems reluctant to go\n What is the same meaning with 'is blowing hot and cold'?", "keeps talking about trivial things", "keeps going", "keeps changing her mood", "keeps making you confused", "C", "1"));
         data.add(new Question(2, "Social networking aims at promoting and aiding communication. _____", "However", "Otherwise", "Moreover", "As a result", "A", "1"));
-        String sql = "INSERT INTO " + QUES_TABLE + "(ques, ans_a, ans_b, ans_c, ans_d, correct_ans, level) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
         for (Question i: data) {
-            String[] args = {i.getQues(), i.getAnswer_a(), i.getAnswer_b(), i.getAnswer_b(), i.getAnswer_c(), i.getAnswer_d(), i.getCorrect_ans(), i.getLevel()};
+            String sql = "INSERT INTO " + QUES_TABLE + "(ques, ans_a, ans_b, ans_c, ans_d, correct_ans, level) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            String[] args = {i.getQues(), i.getAnswer_a(), i.getAnswer_b(), i.getAnswer_c(), i.getAnswer_d(), i.getCorrect_ans(), i.getLevel()};
             st.execSQL(sql, args);
         }
+        st.close();
     }
 
     @Override
@@ -55,26 +57,27 @@ public class SQliteQuestionHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        db.execSQL("DROP TABLE IF EXISTS " + QUES_TABLE);
+//        onCreate(db);
+    }
 
     public Question getQues(int id) {
-        String sql = "SELECT * FROM " + QUES_TABLE + " WHERE id = ? LIMIT 1";
-        String[] args = {String.valueOf(id)};
         SQLiteDatabase st = getReadableDatabase();
-        Cursor rs = st.rawQuery(sql, args);
-        rs.moveToFirst();
-        int ques_id = rs.getInt(0);
-        String ques = rs.getString(1);
-        String ans_a = rs.getString(2);
-        String ans_b = rs.getString(3);
-        String ans_c = rs.getString(4);
-        String ans_d = rs.getString(5);
-        String correct_ans = rs.getString(6);
-        String level = rs.getString(8);
-        return new Question(ques_id, ques, ans_a, ans_b, ans_c, ans_d, correct_ans, level);
-//        while(rs != null && rs.moveToNext()) {
-//
-//        }
+        Question i = null;
+        Cursor rs = st.rawQuery("SELECT * FROM question WHERE id=? LIMIT 1", new String[]{String.valueOf(id)});
+        while (rs != null && rs.moveToNext()) {
+            int ques_id = rs.getInt(0);
+            String ques = rs.getString(1);
+            String ans_a = rs.getString(2);
+            String ans_b = rs.getString(3);
+            String ans_c = rs.getString(4);
+            String ans_d = rs.getString(5);
+            String correct_ans = rs.getString(6);
+            String level = rs.getString(8);
+            i = new Question(ques_id, ques, ans_a, ans_b, ans_c, ans_d, correct_ans, level);
+        }
+        return i;
     }
 
     public long getQuesAmount() {
--- app/src/main/java/com/example/hpl_one/Student/QuesActivity.java
+++ app/src/main/java/com/example/hpl_one/Student/QuesActivity.java
@@ -4,52 +4,117 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.hpl_one.Modules.Question;
 import com.example.hpl_one.R;
 import com.example.hpl_one.SQLite.SQliteQuestionHelper;
 
-public class QuesActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuesActivity extends AppCompatActivity {
     private TextView ques;
-    private AppCompatButton ans_a, ans_b, ans_c, ans_d;
-    private long totalQues = 0;
+    private AppCompatButton ans_a, ans_b, ans_c, ans_d, next;
+    private ImageView main_sc;
     private int currentQuesId = 0;
-    private SQliteQuestionHelper db;
+    private List<Question> data = new ArrayList<>();
     private Question x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ques);
+        initQues();
+        initVar();
+        handlerEvent();
+    }
+
+    private void handlerEvent() {
+        ans_a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAns("A");
+            }
+        });
+
+        ans_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAns("B");
+            }
+        });
+
+        ans_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAns("C");
+            }
+        });
+
+        ans_d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAns("D");
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((currentQuesId < data.size()-1)) {
+                    showQues(++currentQuesId);
+                } else {
+                    showQues(0);
+                }
+            }
+        });
+
+        main_sc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuesActivity.this, StudentActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void initQues() {
+        if (data != null) data.clear();
+        data.add(new Question(0, "In many western cultures, the White Lily is a flower that is representative of purity and sweetness\n What is the same meaning with 'representative'?", "advantageous", "marvelous", "symbolic", "universal", "C", "1"));
+        data.add(new Question(1, "I can’t figure out Daisy’s attitude toward the trip – she is blowing hot and cold about it. First she’s all excited, but then she seems reluctant to go\n What is the same meaning with 'is blowing hot and cold'?", "keeps talking about trivial things", "keeps going", "keeps changing her mood", "keeps making you confused", "C", "1"));
+        data.add(new Question(2, "Social networking aims at promoting and aiding communication. _____", "However", "Otherwise", "Moreover", "As a result", "A", "1"));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        initVar();
     }
 
     private void initVar() {
-        //init DB
-        db = new SQliteQuestionHelper(getApplicationContext());
-
         ques        = findViewById(R.id.student_ques);
         ans_a       = findViewById(R.id.ans_a);
         ans_b       = findViewById(R.id.ans_b);
         ans_c       = findViewById(R.id.ans_c);
         ans_d       = findViewById(R.id.ans_d);
+        next        = findViewById(R.id.ques_next);
+        main_sc     = findViewById(R.id.main_sc);
 
         //Get amount of ques
-        totalQues = db.getQuesAmount();
         showQues(currentQuesId);
     }
 
     private void showQues(int id) {
-        x = db.getQues(id);
+        ans_a.setEnabled(true);
+        ans_b.setEnabled(true);
+        ans_c.setEnabled(true);
+        ans_d.setEnabled(true);
+        x = data.get(id);
         ques.setText(x.getQues());
         ans_a.setText("A. "+x.getAnswer_a());
         ans_b.setText("B. "+x.getAnswer_b());
@@ -61,33 +126,11 @@ public class QuesActivity extends AppCompatActivity implements View.OnClickListe
         ans_d.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ans));
     }
 
-    @Override
-    public void onClick(View v) {
-        String choose;
-        switch (v.getId()) {
-            case R.id.ans_a:
-                checkAns("A");
-                break;
-            case R.id.ans_b:
-                checkAns("B");
-                break;
-            case R.id.ans_c:
-                checkAns("C");
-                break;
-            case R.id.ans_d:
-                checkAns("D");
-                break;
-            case R.id.ques_next:
-                if ((currentQuesId < totalQues)) {
-                    showQues(++currentQuesId);
-                } else {
-                    showQues(0);
-                }
-                break;
-        }
-    }
-
     private void checkAns(String ans) {
+        ans_a.setEnabled(false);
+        ans_b.setEnabled(false);
+        ans_c.setEnabled(false);
+        ans_d.setEnabled(false);
         switch (x.getCorrect_ans()) {
             case "A":
                 ans_a.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.correct_ans));
