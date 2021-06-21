466cf9179467503b3d6a03bd0d235fb43209b5ed
DauMoe <lehuyhoang1111999@gmail.com>
2021-06-21 10:50:04 +0700

    (DONING) student mode

--- app/src/main/java/com/example/hpl_one/SQLite/SQliteQuestionHelper.java
+++ app/src/main/java/com/example/hpl_one/SQLite/SQliteQuestionHelper.java
@@ -41,6 +41,7 @@ public class SQliteQuestionHelper extends SQLiteOpenHelper {
         SQLiteDatabase st = getWritableDatabase();
         data.add(new Question(0, "In many western cultures, the White Lily is a flower that is representative of purity and sweetness\n What is the same meaning with 'representative'?", "advantageous", "marvelous", "symbolic", "universal", "C", "1"));
         data.add(new Question(1, "I can’t figure out Daisy’s attitude toward the trip – she is blowing hot and cold about it. First she’s all excited, but then she seems reluctant to go\n What is the same meaning with 'is blowing hot and cold'?", "keeps talking about trivial things", "keeps going", "keeps changing her mood", "keeps making you confused", "C", "1"));
+        data.add(new Question(2, "Social networking aims at promoting and aiding communication. _____", "However", "Otherwise", "Moreover", "As a result", "A", "1"));
         String sql = "INSERT INTO " + QUES_TABLE + "(ques, ans_a, ans_b, ans_c, ans_d, correct_ans, level) VALUES (?, ?, ?, ?, ?, ?, ?)";
         for (Question i: data) {
             String[] args = {i.getQues(), i.getAnswer_a(), i.getAnswer_b(), i.getAnswer_b(), i.getAnswer_c(), i.getAnswer_d(), i.getCorrect_ans(), i.getLevel()};
