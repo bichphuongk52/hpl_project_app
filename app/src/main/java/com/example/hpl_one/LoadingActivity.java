a59f6edd92042ce4e196efc185062ef7ed670874
DauMoe <lehuyhoang1111999@gmail.com>
2021-06-22 16:20:30 +0700

    (DONE) Student Mode

--- app/src/main/java/com/example/hpl_one/LoadingActivity.java
+++ app/src/main/java/com/example/hpl_one/LoadingActivity.java
@@ -16,21 +16,34 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         pref = getSharedPreferences(Config.LOGIN_STATE, MODE_PRIVATE);
-        try {
-            Thread.sleep(3500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        new CheckLogined().start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private class CheckLogined extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //Check user login or not
+            if (pref.contains(Config.LOGINED) && Boolean.valueOf(pref.getString(Config.LOGINED, null))) {
+                //if login state is saved in SharePreferences
+                Intent student_intent = new Intent(LoadingActivity.this, StudentActivity.class);
+                student_intent.putExtra("username", String.valueOf(pref.getString(Config.LOGINED, null)));
+                startActivity(student_intent);
+            } else {
+                //if user not login or state is not exist
+                startActivity(new Intent(LoadingActivity.this, LoginActivity.class));
+            }
+            finish();
         }
-        //Check user login or not
-        if (pref.contains(Config.LOGINED) && Boolean.valueOf(pref.getString(Config.LOGINED, null))) {
-            //if login state is saved in SharePreferences
-            Intent student_intent = new Intent(LoadingActivity.this, StudentActivity.class);
-            student_intent.putExtra("username", String.valueOf(pref.getString(Config.LOGINED, null)));
-            startActivity(student_intent);
-        } else {
-            //if user not login or state is not exist
-            startActivity(new Intent(LoadingActivity.this, LoginActivity.class));
-        }
-        finish();
     }
 }
